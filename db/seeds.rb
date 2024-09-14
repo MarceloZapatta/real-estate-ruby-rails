05184805656397a1602be2378c98ad3100ba7142
Marcelo Zapatta <mzapatta@insiderealestate.com>
2024-09-13 23:09:57 -0300

    feat: user seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(name: 'Admin', email: 'admin@company.com', active: true, password: '12345678', password_confirmation: '12345678')
